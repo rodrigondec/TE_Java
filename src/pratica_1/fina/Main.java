7eda89057308b36aa0de1331ecf75c13ac5eb443
RodrigoNdeC <rodrigondec@gmail.com>
2018-03-21 19:08:45 -0300

    finally feito

---
+++ src/pratica_1/fina/Main.java
@@ -0,0 +1,13 @@
+package pratica_1.fina;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Try.");
+        }
+        finally {
+            System.out.println("Finally.");
+        }
+    }
+}
